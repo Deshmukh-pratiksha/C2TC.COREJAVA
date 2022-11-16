175fdcc6f0a2e37fea1867b04a3cbe79af31ae7c
aditya com <aditya com@DESKTOP-HN61KEL>
2022-11-16 10:13:06 -0800

    This is my fourth commit

---
+++ JavaDemo/src/com/tnsif/collectionEx/IteratorDemo.java
@@ -0,0 +1,33 @@
+package com.tnsif.collectionEx;
+
+import java.util.LinkedList;
+
+import java.util.Iterator;
+
+public class IteratorDemo {
+	
+	void display(LinkedList<String>list) {
+		Iterator<String> it=list.iterator();
+		while(it.hasNext()) 
+		{
+		  System.out.println("Player name is:"+it.next());
+		}
+	}
+
+	public static void main(String[]args) {
+		LinkedList<String>player = new LinkedList<String>();
+		
+		
+		player.add("Dhoni");
+		player.add("Rohit");
+		player.add("Yuvraj");
+		player.add("Virat");
+		player.add("Surya");
+		
+		
+		System.out.println(player);
+		IteratorDemo im=new IteratorDemo();
+		im.display(player);
+	
+	}
+}
