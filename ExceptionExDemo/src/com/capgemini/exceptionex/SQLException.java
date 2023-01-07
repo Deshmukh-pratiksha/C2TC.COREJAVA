3143339a28d590df0ad973483e1694e1be366f26
aditya com <aditya com@DESKTOP-HN61KEL>
2023-01-06 22:17:03 -0800

    This is the commit for ExceptionExDemo

---
+++ ExceptionExDemo/src/com/capgemini/exceptionex/SQLException.java
@@ -0,0 +1,5 @@
+package com.capgemini.exceptionex;
+
+public class SQLException extends Exception {
+
+}
---
+++ ExceptionExDemo/src/com/capgemini/exceptionex/ThrowEx.java
@@ -0,0 +1,28 @@
+package com.capgemini.exceptionex;
+
+public class ThrowEx{
+	void validate(int age)
+	{
+		try {
+			if(age>16)
+			{
+				System.out.println("Student is eligible");
+			}
+			else
+			{
+				throw new ArithmeticException("Student can not appear for exam");
+				//System.out.println("Student is not Eligible");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[]args) {
+		
+		ThrowEx obj=new ThrowEx();
+		obj.validate(18);
+		System.out.println("Hello");
+	}
+}
